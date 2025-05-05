9d0c0f43fe638b38fc0d29ca651bf02a1064d0fe
swethamukuntharajan <swethamrajan94@gmail.com>
2025-05-05 17:29:26 +0530

    Initial project Finance commit

---
+++ finance/src/main/java/com/personal/finance/FinanceMadeEasyApplication.java
@@ -0,0 +1,13 @@
+package com.personal.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceMadeEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceMadeEasyApplication.class, args);
+	}
+
+}
